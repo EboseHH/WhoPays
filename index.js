37d12dd0a728eb58974ebd317168499e33ae972d
Ebose <o.ebosetale@gmail.com>
2023-02-18 20:03:34 +0100

    Implemented the results page and made design changes to the buttons

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //create an array to hold the names from the form inputs
 const entries = [];
 
+// this function removes the class "active" from every element that has it
+// using "querySelectorAll" and "forEach" and then adds the class active to
+// the target element which has the id in sectionName
 function setActiveSection(sectionName) {
   document.querySelectorAll(".active").forEach((element) => {
     element.classList.remove("active");
   });
   document.querySelector(`#${sectionName}`).classList.add("active");
 }
-
 setActiveSection("landing");
 
 // add an event listener to the forms for when it is submitted
@@ -34,19 +36,29 @@ document.querySelector("#names").addEventListener("submit", (event) => {
   // using the map function (entries.map) which takes in the submitted names and applies
   // the function to generate a div with the specified classes and name
   const elements = entries.map((name) => {
-    return `<div class="submittedName inline-block ml-3 bg-amber-300 px-2 py-1">${name}</div>`;
+    return `<div class="submittedName inline-block ml-3 bg-amber-300 rounded-full px-3 py-1">${name}</div>`;
   });
 
   // display each submitted name
   document.querySelector("#entries").innerHTML = elements.join("");
 });
 
-// add an event listener to the shuffle button and run the setActiveSection when the button
-// is clicked to display the loading page
-document.querySelector("#shuffle").addEventListener("click", (event) => {
+// this function selects a random name, displays the loading page
+// and then displays the result after the timeout
+function shuffle() {
+  const randomNameIndex = Math.floor(Math.random() * entries.length);
+  const randomName = entries[randomNameIndex];
   setActiveSection("loading");
   // set a time out on the loading page and display the results page after 2000 milliseconds
   setTimeout(() => {
+    document.querySelector("#instruction").innerHTML =
+      "We've selected one person to handle the bills";
+    document.querySelector("#randomName").innerHTML = randomName;
     setActiveSection("result");
-  }, 3000);
-});
+  }, 2000);
+}
+// add an event listener to both the shuffle and shuffleAgain button
+// when either button is clicked to display the loading page, select a random
+// name from the entries and display the result page
+document.querySelector("#shuffle").addEventListener("click", shuffle);
+document.querySelector("#shuffleAgain").addEventListener("click", shuffle);
